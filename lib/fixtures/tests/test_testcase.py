a6f79f5c8135e9b54fa963c23614d6be5404bb9e
Gavin Panella <gavin@gromper.net>
2011-06-17 22:43:11 +0100

    Gather details from fixtures that fail to setUp() in TestWithFixtures.

--- lib/fixtures/tests/test_testcase.py
+++ lib/fixtures/tests/test_testcase.py
@@ -15,8 +15,12 @@
 
 import unittest
 import testtools
+from testtools.content import text_content
+from testtools.testcase import skipIf
 
 import fixtures
+from fixtures import TestWithFixtures
+from fixtures.fixture import gather_details
 from fixtures.tests.helpers import LoggingFixture
 
 
@@ -24,7 +28,7 @@ class TestTestWithFixtures(unittest.TestCase):
 
     def test_useFixture(self):
         fixture = LoggingFixture()
-        class SimpleTest(testtools.TestCase, fixtures.TestWithFixtures):
+        class SimpleTest(testtools.TestCase, TestWithFixtures):
             def test_foo(self):
                 self.useFixture(fixture)
         result = unittest.TestResult()
@@ -38,10 +42,56 @@ class TestTestWithFixtures(unittest.TestCase):
             calls.append('called')
             raise Exception('foo')
         fixture = fixtures.FunctionFixture(lambda:None, raiser)
-        class SimpleTest(testtools.TestCase, fixtures.TestWithFixtures):
+        class SimpleTest(testtools.TestCase, TestWithFixtures):
             def test_foo(self):
                 self.useFixture(fixture)
         result = unittest.TestResult()
         SimpleTest('test_foo').run(result)
         self.assertFalse(result.wasSuccessful())
         self.assertEqual(['called'], calls)
+
+    @skipIf(gather_details is None, "gather_details() is not available.")
+    def test_useFixture_details_captured_from_setUp(self):
+        # Details added during fixture set-up are gathered even if setUp()
+        # fails with an exception.
+        class SomethingBroke(Exception): pass
+        class BrokenFixture(fixtures.Fixture):
+            def setUp(self):
+                super(BrokenFixture, self).setUp()
+                self.addDetail('content', text_content("foobar"))
+                raise SomethingBroke()
+        broken_fixture = BrokenFixture()
+        class DetailedTestCase(TestWithFixtures, testtools.TestCase):
+            def setUp(self):
+                super(DetailedTestCase, self).setUp()
+                self.useFixture(broken_fixture)
+            def test(self):
+                pass
+        detailed_test_case = DetailedTestCase("test")
+        self.assertRaises(SomethingBroke, detailed_test_case.setUp)
+        self.assertEqual(
+            {"content": text_content("foobar")},
+            broken_fixture.getDetails())
+        self.assertEqual(
+            {"content": text_content("foobar")},
+            detailed_test_case.getDetails())
+
+    @skipIf(gather_details is None, "gather_details() is not available.")
+    def test_useFixture_details_not_captured_from_setUp(self):
+        # Details added during fixture set-up are not gathered if the test
+        # case does not have the ability to accept those details.
+        class SomethingBroke(Exception): pass
+        class BrokenFixture(fixtures.Fixture):
+            def setUp(self):
+                super(BrokenFixture, self).setUp()
+                self.addDetail('content', text_content("foobar"))
+                raise SomethingBroke()
+        broken_fixture = BrokenFixture()
+        class NonDetailedTestCase(TestWithFixtures, unittest.TestCase):
+            def setUp(self):
+                super(NonDetailedTestCase, self).setUp()
+                self.useFixture(broken_fixture)
+            def test(self):
+                pass
+        non_detailed_test_case = NonDetailedTestCase("test")
+        self.assertRaises(SomethingBroke, non_detailed_test_case.setUp)
