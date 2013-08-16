f0e8391efaf1b2143fac94647bdf3ccad91cb13e
Robert Collins <robertc@robertcollins.net>
2013-08-16 12:53:51 +1200

    Release 0.3.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os.path
 description = open(os.path.join(os.path.dirname(__file__), 'README'), 'rt').read()
 
 setup(name="fixtures",
-      version="0.3.12",
+      version="0.3.13",
       description="Fixtures, reusable state for writing clean tests and more.",
       keywords="fixture fixtures unittest contextmanager",
       long_description=description,
