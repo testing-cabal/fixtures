aa6f5e0d92bddc65773c99f4ed5bb4b17248bdda
hugovk <hugovk@users.noreply.github.com>
2017-10-13 08:41:11 +0300

    Drop support for EOL Python 2.6

--- fixtures/tests/__init__.py
+++ fixtures/tests/__init__.py
@@ -1,12 +1,12 @@
 #  fixtures: Fixtures with cleanups for testing and convenience.
 #
 # Copyright (c) 2010, Robert Collins <robertc@robertcollins.net>
-# 
+#
 # Licensed under either the Apache License, Version 2.0 or the BSD 3-clause
 # license at the users choice. A copy of both licenses are available in the
 # project source as Apache-2.0 and BSD. You may not use this file except in
 # compliance with one of these two licences.
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under these licenses is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -14,7 +14,6 @@
 # limitations under that license.
 
 import doctest
-import sys
 import unittest
 
 import fixtures.tests._fixtures
@@ -35,13 +34,8 @@ def load_tests(loader, standard_tests, pattern):
     prefix = "fixtures.tests.test_"
     test_mod_names = [prefix + test_module for test_module in test_modules]
     standard_tests.addTests(loader.loadTestsFromNames(test_mod_names))
-    if sys.version_info >= (2, 7):
-        # 2.7 calls load_tests for us
-        standard_tests.addTests(loader.loadTestsFromName('fixtures.tests._fixtures'))
-    else:
-        # We need to call it ourselves.
-        standard_tests.addTests(fixtures.tests._fixtures.load_tests(
-            loader, loader.loadTestsFromName('fixtures.tests._fixtures'), pattern))
+    standard_tests.addTests(
+        loader.loadTestsFromName('fixtures.tests._fixtures'))
     doctest.set_unittest_reportflags(doctest.REPORT_ONLY_FIRST_FAILURE)
     standard_tests.addTest(doctest.DocFileSuite("../../README"))
     return standard_tests
